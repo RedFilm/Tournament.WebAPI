2ca0b768bb5ff00ccdb64dd90d351dc5dc30f2bf
Volkov Dmitriy <m0nterha@mail.ru>
2024-02-27 15:09:48 +0300

    Add serilog configuration.

--- Tournaments.API/Middlewares/ExceptionHandlerMiddleware.cs
+++ Tournaments.API/Middlewares/ExceptionHandlerMiddleware.cs
@@ -9,11 +9,13 @@ namespace Tournaments.API.Middlewares
 	public class ExceptionHandlerMiddleware
 	{
 		private readonly RequestDelegate _next;
+		private readonly ILogger<ExceptionHandlerMiddleware> _logger;
 
-		public ExceptionHandlerMiddleware(RequestDelegate next)
-        {
-            _next = next;
-        }
+		public ExceptionHandlerMiddleware(RequestDelegate next, ILogger<ExceptionHandlerMiddleware> logger)
+		{
+			_next = next;
+			_logger = logger;
+		}
 
 		public async Task Invoke(HttpContext context)
 		{
